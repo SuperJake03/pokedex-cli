8c8ebccb580605e7faf53ab13c02789fbcaa1c5f
Superjake03 <jacoblopez0308@gmail.com>
2026-07-02 17:47:12 -0500

    statRepl()

--- repl.go
+++ repl.go
@@ -1,6 +1,26 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
